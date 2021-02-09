793d2f87d1f2a5003608046932e20a8407445108
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-09 15:48:13 +0530

    Basic floating menu added (Icons issue not fixed)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
 
+// Import Components
+import FloatingMenu from "./Components/floatingMenu/FloatingMenu.js";
+
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <FloatingMenu />
     </div>
   );
 }
