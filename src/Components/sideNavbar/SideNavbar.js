8b2281dbb48bc981b430eb1bc0487b5a8b5a091c
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-10 10:08:56 +0530

    Side nav bar components added

--- src/Components/sideNavbar/SideNavbar.js
+++ src/Components/sideNavbar/SideNavbar.js
@@ -1,15 +1,92 @@
-import React, { Component } from "react";
+import React, { useState, forwardRef } from "react";
+import { NavLink as RouterLink } from "react-router-dom";
 
-class SideNavbar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// Material UI Components
+import { List, ListItem, Collapse, Button, Drawer } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
 
-  render() {
-    return (
-      <div>
-        <div />
-      </div>
-    );
-  }
-}
+//classnames alternative modules
+import clsx from "clsx";
+
+import useStyles from "./menuBarStyles";
+// Menu data import
+import MenuData from "./menuData.js";
+
+const MenuBar = props => {
+  const [menu, setMenu] = useState({});
+  const { className, ...rest } = props;
+  const classes = useStyles();
+  const handleClick = item => {
+    let newData = { ...menu, [item]: !menu[item] };
+    setMenu(newData);
+  };
+  const CustomRouterLink = forwardRef((props, ref) => (
+    <div ref={ref} style={{ flexGrow: 1 }}>
+      <RouterLink {...props} />
+    </div>
+  ));
+  const handleMenu = (children, level = 0) => {
+    return children.map(({ children, name, url, links }) => {
+      if (!children) {
+        return (
+          <List component="div" disablePadding key={name}>
+            <ListItem
+              className={classes.item}
+              disableGutters
+              style={{ padding: "0px" }}
+              key={name}
+            >
+              <Button
+                className={clsx({
+                  [classes.btnRoot]: true,
+                  [classes.button]: true,
+                  [classes.subMenu]: level
+                })}
+                component={CustomRouterLink}
+                to={url}
+              >
+                {name}
+              </Button>
+            </ListItem>
+          </List>
+        );
+      }
+      return (
+        <div key={name}>
+          <ListItem
+            className={classes.item}
+            disableGutters
+            key={name}
+            onClick={() => handleClick(name)}
+          >
+            <Button
+              className={clsx({
+                [classes.btnRoot]: true,
+                [classes.button]: true,
+                [classes.subMenu]: level
+              })}
+            >
+              {name} {menu[name] ? <ExpandLess /> : <ExpandMore />}
+            </Button>
+          </ListItem>
+          <Collapse in={menu[name] ? true : false} timeout="auto" unmountOnExit>
+            {handleMenu(children, 1)}
+          </Collapse>
+        </div>
+      );
+    });
+  };
+  return (
+    <Drawer
+      anchor="left"
+      classes={{ paper: classes.drawer }}
+      open={true}
+      variant="persistent"
+    >
+      <List {...rest} className={clsx(classes.root, className)}>
+        {handleMenu(MenuData)}
+      </List>
+    </Drawer>
+  );
+};
+export default MenuBar;
