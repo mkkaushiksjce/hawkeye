395c1d5a45248eec69d76316e8d4355711b129e7
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-10 08:59:55 +0530

    1 file added

---
+++ src/Components/sideNavbar/SideNavbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SideNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
